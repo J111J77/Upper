f6c04cac4f0538d7d498175e7058e318cef6f308
J111J77 <11177@portalcedi.edu.mx>
2024-04-09 09:50:07 -0600

    xd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home.page',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items: { text: string, checked: boolean, editMode: boolean }[] = [];
 
   constructor() {}
 
+  addItem() {
+    this.items.push({ text: 'New Item', checked: false, editMode: false });
+  }
+
+  removeItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  toggleEditMode(item: { text: string, checked: boolean, editMode: boolean }) {
+    item.editMode = !item.editMode;
+
+  }
 }
+
